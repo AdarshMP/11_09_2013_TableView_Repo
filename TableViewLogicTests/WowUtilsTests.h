849dd28ba6f2af6d512665802c31cf4e58ad0c27
pushpa <nc.pushpa@gmail.com>
2013-09-11 17:58:02 +0530

    "adding test target"

---
+++ TableViewLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  TableviewExample
+//
+//  Created by awsuser8 on 11/09/13.
+//  Copyright (c) 2013 aws. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+    
+@end
